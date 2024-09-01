5bfb4b7cab0dd53b8cf23a967987bcff00772c01
Dasantha Edirisinghe <114149683+Tharindu-Dasantha@users.noreply.github.com>
2024-09-01 07:47:44 +0530

    navigation bug fix
    Finally starts working

---
+++ Compass/src/screens/home/HomeScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface HomeScreenProps {
+    // Define your props here
+}
+
+const HomeScreen: React.FC<HomeScreenProps> = () => {
+    return (
+        <div>
+            {/* Your component content goes here */}
+            <p>Hello world</p>
+        </div>
+    );
+};
+
+export default HomeScreen;
